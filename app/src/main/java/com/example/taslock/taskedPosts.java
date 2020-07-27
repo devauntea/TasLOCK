649e9fcf00c8224fc789422b83f98f66872da5e5
devauntea <devauntea.thompson@csedge.com>
2020-07-27 10:12:43 -0400

    pages

--- app/src/main/java/com/example/taslock/taskedPosts.java
+++ app/src/main/java/com/example/taslock/taskedPosts.java
@@ -2,6 +2,7 @@ package com.example.taslock;
 
 public class taskedPosts {
     String Title; //username for the authenticated user
+    String Teacher;
     int Time; //the text of the message being sent
 
     public taskedPosts() {
@@ -11,6 +12,7 @@ public class taskedPosts {
     public taskedPosts(String Title, String PostMsg) {
         this.Title = Title;
         this.Time = Time;
+        this.Teacher = Teacher;
     }
 
     public String getTitle() {
@@ -21,11 +23,22 @@ public class taskedPosts {
         this.Title = Title;
     }
 
+
+    public String getTeacher() {
+        return Teacher;
+    }
+
+    public void setTeacher(String Title) {
+        this.Title = Teacher;
+    }
+
     public int getPostMsg() {
         return Time;
     }
 
-    public void setPostMsg(String PostMsg) {
+    public void setPostMsg(int PostMsg) {
         this.Time = Time;
     }
+
+
 }
