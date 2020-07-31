068d97aa29c876c44b9ea8b6a2ddc74084aa9939
devauntea <devauntea.thompson@csedge.com>
2020-07-31 10:14:48 -0400

    pages

--- app/src/main/java/com/example/taslock/LognPage.java
+++ app/src/main/java/com/example/taslock/LognPage.java
@@ -9,6 +9,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,7 +19,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class LognPage extends AppCompatActivity {
     EditText pass, email;
-    Button signinBtn;
+    TextView signinBtn;
     FirebaseAuth fAuth;
 
     @Override
@@ -60,4 +61,6 @@ public class LognPage extends AppCompatActivity {
             }
         });
     }
+
+
 }
--- app/src/main/java/com/example/taslock/SignUp.java
+++ app/src/main/java/com/example/taslock/SignUp.java
@@ -9,6 +9,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,7 +19,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
     EditText name, pass, email;
-    Button signupBtn;
+    TextView signupBtn;
     FirebaseAuth fAuth;
 
     @Override
