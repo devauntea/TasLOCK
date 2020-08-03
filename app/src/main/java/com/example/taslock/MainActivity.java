471a616ee97350e568600d67da4443169784ab08
devauntea <devauntea.thompson@csedge.com>
2020-08-02 23:44:01 -0400

    pages

--- app/src/main/java/com/example/taslock/MainActivity.java
+++ app/src/main/java/com/example/taslock/MainActivity.java
@@ -59,11 +59,11 @@ public class MainActivity extends AppCompatActivity {
     /*public void test(View view) {
         Intent intent = new Intent(this, Tasks.class);
         startActivity(intent);
-    }
-    public void tests(View view) {
-        Intent intent = new Intent(this, tasksFack.class);
-        startActivity(intent);
     }*/
+    public void plans(View view) {
+        Intent intent = new Intent(this, Plans.class);
+        startActivity(intent);
+    }
     public void goSignUp(View view){
         Intent intent = new Intent(this, SignUp.class);
         startActivity(intent);
